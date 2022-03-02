21f462f546df0d29015d0930f76bf10cb22a3945
William Surya Jaya <williamsj26.wsj@gmail.com>
2022-03-02 16:06:34 +0700

    Update config for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/Vue-URL_Shortener/' : '/',
   devServer: {
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://vue-url-shortener-api.herokuapp.com/',
         changeOrigin: true,
         secure: false,
       },
