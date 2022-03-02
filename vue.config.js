ec088d77bbaa327af7fdd3e4d4c1cedc64111a55
William Surya Jaya <williamsj26.wsj@gmail.com>
2022-03-02 16:24:08 +0700

    Turning off port

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/Vue-URL_Shortener/' : '/',
   devServer: {
-    port: 8080,
+    // port: 8080,
     proxy: {
       '/api': {
         target: 'https://vue-url-shortener-api.herokuapp.com/',
