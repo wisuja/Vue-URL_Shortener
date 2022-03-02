5d23789ed01c7329db0050ddea2ad7538ef6cfb8
William Surya Jaya <williamsj26.wsj@gmail.com>
2022-03-02 15:20:16 +0700

    Final

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createApp(App).use(router).mount('#app');
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+};
